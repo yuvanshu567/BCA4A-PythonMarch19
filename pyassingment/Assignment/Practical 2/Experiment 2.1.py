03f762c2db5d903422dfa11a2a5cbe3132546b71
yuvanshu567 <128624159+yuvanshu567@users.noreply.github.com>
2024-03-31 15:11:46 +0530

    Update Experiment 2.1.py

--- pyassingment/Assignment/Practical 2/Experiment 2.1.py
+++ pyassingment/Assignment/Practical 2/Experiment 2.1.py
@@ -1,7 +1,7 @@
 #WAP to demonstrate Slicing Operations in Strings
 print("Name:Yuvanshu Bansal\nRoll No: 2210997279")
 
-str = "Agamjot Singh"
+str = "Yuvanshu Bansal"
 print(str[0:8])
 print(str[8:13])
 print(str[0:13])
